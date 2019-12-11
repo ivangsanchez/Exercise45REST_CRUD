77a8673309bf0ca80470bc1a780822864a56770b
Mabestron <ivangamaliel555@hotmail.com>
2019-12-11 13:35:11 -0600

    rest crud 2

--- src/com/Exercise45/services/DAOCustomerImplement.java
+++ src/com/Exercise45/services/DAOCustomerImplement.java
@@ -1,14 +1,18 @@
 package com.Exercise45.services;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Properties;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 
@@ -21,6 +25,8 @@ public class DAOCustomerImplement implements DAOCustomer {
 	Connection conn=null;
 	PreparedStatement pstmnt=null;
 	ResultSet rs=null;
+	String sentenciaSQL="";
+	int rowsaffected=0;
 	
 	private Connection getConnection() {
 		Connection conn=null;
@@ -64,9 +70,52 @@ public class DAOCustomerImplement implements DAOCustomer {
 	}
 
 	@Override
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
 	public boolean UpdateCustomer(Customer myCustomer) {
 		// TODO Auto-generated method stub
-		return false;
+		conn = getConnection();
+		Properties props = new Properties();
+		InputStream in = this.getClass().getClassLoader().getResourceAsStream("dao.properties");
+		
+		try {
+			props.load(in);
+			if(props!=null) {
+				sentenciaSQL=props.getProperty("SQLUpdateCustomer");
+			}
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+		try {
+			pstmnt=conn.prepareStatement(sentenciaSQL);
+			pstmnt.setString(1, myCustomer.getNameCustomer());
+			pstmnt.setString(2, myCustomer.getAddressCustomer());
+			pstmnt.setInt(3, myCustomer.getAgeCustomer());
+			pstmnt.setDouble(4, myCustomer.getHeightCustomer());
+			pstmnt.setDouble(5, myCustomer.getWeightCustomer());
+			pstmnt.setBoolean(6, myCustomer.getIsSingle());
+			pstmnt.setInt(7, myCustomer.getIdCustomer());
+			rowsaffected = pstmnt.executeUpdate();
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				pstmnt.close();
+				conn.close();
+				
+			}
+			catch(SQLException e){
+				e.printStackTrace();
+			}
+		}
+		
+		return (rowsaffected!=0)?true:false;
 	}
 
 	@Override
@@ -77,32 +126,7 @@ public class DAOCustomerImplement implements DAOCustomer {
 
 	@Override
 	public Customer ReadCustomer(int idCustomer) {
-		
-		String sentenciaSQL = "SELECT * FROM Customer";
-		conn =getConnection();
-		
-		try {
-			pstmnt=conn.prepareStatement(sentenciaSQL);
-			pstmnt.executeUpdate();
-			
-			System.out.println("Record READ from database");
-		} catch (SQLException e) {
 
-			e.printStackTrace();
-		}
-		finally {
-			
-			try {
-				pstmnt.close();
-				conn.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-		}
-
-		
 		return null;
 	}
 
